9e097d565790d45f427d33fe6f9048a657ea2358
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-06-09 18:48:37 -0300

    Controlando Formulário  📦

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,9 +3,20 @@ import Botao from '../Botao';
 import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
+    state = {
+        tarefa:"",
+        tempo:"00:00"
+    }
+
+    adicionarTarefa(evento : React.FormEvent){ // evitar que o button atualize a pagina quando apertada
+        evento.preventDefault(); 
+        console.log('state', this.state)
+    }
+
+
     render(){
-        return ( //  <div className={style["novaTarefa__-container"]}> outra maneira de adicionar o objeto com "-"
-            <form className={style.novaTarefa}> 
+        return ( // erro de associação do escopo do class component ex: nomedafunção.bind(this)
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}> 
                 <div className={style.inputContainer}> 
                     <label htmlFor="tarefa">
                         adicione um novo estudo
@@ -13,6 +24,8 @@ class Formulario extends React.Component {
                     <input
                         type="text"
                         name="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa:evento.target.value})}
                         id="tarefa"
                         placeholder="o que você quer estudar ? "
                         required
@@ -26,6 +39,8 @@ class Formulario extends React.Component {
                     type="time"
                     step="1"
                     name="tempo"
+                    value={this.state.tempo}
+                    onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                     id="tempo"
                     min="00:00:00"
                     max="01:30:00"
