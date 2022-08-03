75e86662b3437745df5438b2faaf140ace2fc841
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-08-02 22:11:56 -0300

    Regressiva no contador  📦

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component <{children : any, type?: "button" | "submit" | "reset" | undefined}>  { // como ja tinha o children, apenas coloquei o tipo de button do lado
+class Botao extends React.Component <{children : any,
+     type?: "button" | "submit" | "reset" | undefined,
+     onClick?: () => void
+
+}
+>  { // como ja tinha o children, apenas coloquei o tipo de button do lado
     render() {
-        const {type = "button"} = this.props
+        const {type = "button", onClick} = this.props
         return (
-            <button type ={type} className={style.Botao}>
+            <button onClick={onClick} type ={type} className={style.Botao}>
                 {this.props.children} 
             </button>
         )  
