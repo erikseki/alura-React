eb2bcc71571fa530dcbe6b8a166cc02520c121c4
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-08-15 14:35:32 -0300

    Finalizando a tarefa  📦

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -16,6 +16,24 @@ function App() {
       selecionado: tarefa.id === tarefaSelecionada.id ? true : false 
     })) )
   }
+
+function finalizarTarefa(){
+  if(selecionado){
+    setSelecionado(undefined)
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+      if(tarefa.id === selecionado.id) {
+        return {
+          ...tarefa,
+          selecionado: false,
+          completado: true
+        } 
+      }
+
+      return tarefa;
+    }))
+  }
+}
+
   return (
     <div className={style.AppStyle}>
     <Formulario setTarefas={setTarefas}/>
@@ -23,7 +41,10 @@ function App() {
       tarefas={tarefas}
       selecionaTarefa={selecionaTarefa}
     />
-    <Cronometro selecionado={selecionado}/>
+    <Cronometro 
+    selecionado={selecionado} 
+    finalizarTarefa={finalizarTarefa}
+    />
     </div>
   );
 }
